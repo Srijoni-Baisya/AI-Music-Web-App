a2033d0922096b8ee878e90d0bfcf75acb5a4767
Srijoni-Baisya <75311639+Srijoni-Baisya@users.noreply.github.com>
2021-10-01 14:56:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+//create 2 variables for storing music
+music_1 = "";
+music_2 = "";
+
+//define preload function to load the 2 songs
+function preload(){
+    music_1 = loadSound("Jamaica Farewell.mp3");
+    music_2 = loadSound("Five Hundred Miles.mp3");
+}
+
+function setup(){
+    //create the canvas
+    canvas = createCanvas(480,480);
+    canvas.center();
+
+    //access the webcam
+    video = createCapture(VIDEO);
+
+    //hiding the extra component
+    video.hide();
+}
+
+function draw(){
+    //place the webcam live view on the canvas
+    image(video,0,0,480,480);
+}
