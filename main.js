8cac4e5b70c12fa08f2fe1919a5b3f4bb9e9a027
Srijoni-Baisya <75311639+Srijoni-Baisya@users.noreply.github.com>
2021-10-01 18:08:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,56 +1,26 @@
-//create an empty variable to store the music
-song = "";
+//create 2 variables for storing music
+music_1 = "";
+music_2 = "";
 
-leftWristX = 0;
-leftWristY = 0;
-
-rightWristX = 0;
-rightWristY = 0;
-
+//define preload function to load the 2 songs
 function preload(){
-    song = loadSound("music.mp3");
+    music_1 = loadSound("music1.mp3");
+    music_2 = loadSound("music_2.mp3");
 }
 
 function setup(){
-    //create canvas
-    canvas = createCanvas(500,500);
+    //create the canvas
+    canvas = createCanvas(400,350);
     canvas.center();
 
     //access the webcam
     video = createCapture(VIDEO);
+
+    //hiding the extra component
     video.hide();
-
-    poseNet = ml5.poseNet(video,modelLoaded);
-
-    poseNet.on('pose',gotPoses);
-}
-
-function modelLoaded(){
-    console.log("PoseNet is initialized!");
 }
 
 function draw(){
-    //place the video
-    image(video,0,0,500,500);
-}
-
-function gotPoses(results){
-    if(results.length > 0){
-        console.log(results);
-
-        leftWristX = results[0].pose.leftWrist.x;
-        leftWristY = results[0].pose.leftWrist.y;
-        console.log("Left Wrist X = " + leftWristX + " , Left Wrist Y = " + leftWristY);
-
-        rightWristX = results[0].pose.rightWrist.x;
-        rightWristY = results[0].pose.rightWrist.y;
-        console.log("Right Wrist X = " + rightWristX + " , Right Wrist Y = " + rightWristY);
-    }
-}
-
-function play(){
-    //play the sound
-    song.play();
-    song.setVolume(1);
-    song.rate(1);
+    //place the webcam live view on the canvas
+    image(video,0,0,400,350);
 }
